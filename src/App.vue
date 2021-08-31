6f59013cd2a38c0943f97feeda0e6407ba7a0920
Fineas Gavre <fineasgavre@gmail.com>
2021-08-31 16:37:08 +0300

    Created button mockup.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
     import HeaderNavbar from "./components/header/navbar/HeaderNavbar.vue"
     import Searchbar from "./components/search/Searchbar.vue";
+import SimpleButton from "./components/buttons/SimpleButton.vue";
 
 </script>
 
@@ -10,6 +11,16 @@
         <div class="main-content">
             <img src="./assets/images/google-logo.png" alt="Google logo" />
             <Searchbar></Searchbar>
+            <div class="button-group">
+                <SimpleButton title="Google Search"></SimpleButton>
+                <SimpleButton title="I'm Feeling Norocos"></SimpleButton>
+            </div>
+            <div class="language-cta">
+                <span>Google offered in:</span>
+                <a href="#">Română</a>
+                <a href="#">magyar</a>
+                <a href="#">Deutsch</a>
+            </div>
         </div>
     </div>
 </template>
@@ -33,5 +44,19 @@
         flex-direction: column;
         align-items: center;
         gap: 2rem;
+    }
+
+    .button-group {
+        display: inline-flex;
+        align-items: center;
+        gap: 1rem;
+    }
+
+    .language-cta {
+        color: #3c4043;
+        font-size: 13px;
+
+        display: inline-flex;
+        gap: 8px;
     }
 </style>
---
+++ src/components/buttons/SimpleButton.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+    const props = defineProps({
+        title: String
+    })
+</script>
+
+<template>
+    <button class="simple-button">
+        {{ props.title }}
+    </button>
+</template>
+
+<style scoped>
+    .simple-button {
+        font-size: 14px;
+
+        color: #3c4043;
+        background-color: #f8f9fa;
+        border: 0;
+        border-radius: 4px;
+
+        padding-top: 10px;
+        padding-bottom: 10px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+
+    .simple-button:hover {
+        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.10);
+        border: 1px solid #dadce0;
+
+        padding-top: 9px;
+        padding-bottom: 9px;
+        padding-left: 19px;
+        padding-right: 19px;
+    }
+</style>
