79c3fb772bc21eb9d762a734d9ca5fd860f2e0d2
Fineas Gavre <fineasgavre@gmail.com>
2021-08-31 15:26:39 +0300

    Removed default Vite / Vue 3 scaffold content and replaced with WIP Header Navbar.

---
+++ src/components/header/navbar/HeaderNavbar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import TextHeaderItem from "./TextHeaderItem.vue"
+
+</script>
+
+<template>
+    <div class="header-wrapper">
+        <TextHeaderItem href="#" title="Gmail"></TextHeaderItem>
+        <TextHeaderItem href="#" title="Images"></TextHeaderItem>
+        <TextHeaderItem href="#" title="test"></TextHeaderItem>
+    </div>
+</template>
+
+<style scoped>
+    .header-wrapper {
+        align-self: flex-end;
+
+        height: 3rem;
+        margin-right: 1rem;
+
+        display: inline-flex;
+        align-items: center;
+        gap: 1rem;
+    }
+</style>
---
+++ src/components/header/navbar/TextHeaderItem.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    const props = defineProps({
+        title: String,
+        href: String
+    })
+</script>
+
+<template>
+    <a :href="props.href" class="header-item">
+        {{ props.title }}
+    </a>
+</template>
+
+<style>
+    .header-item {
+        color: rgba(0, 0, 0, 0.71);
+        line-height: 24px;
+        vertical-align: middle;
+        font-size: 13px;
+        text-decoration: none;
+    }
+
+    .header-item:hover {
+        text-decoration: underline !important;
+    }
+</style>
